25e5d483251906336d0a7482725ece4eddc0aac9
unknown <kwlazo9@gmail.com>
2020-07-02 13:00:57 +0200

    upload maven project

---
+++ src/test/java/karol/spring/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package karol.spring.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
